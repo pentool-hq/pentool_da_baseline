a193fb47ea7856c92cbfc3004cd55900c519d075
Jerry Wong <yuki.n@tuta.io>
2024-10-13 14:52:58 +0800

    Refactor (#1)
    * add sample vercel config
    
    * vercel
    
    * vercel
    
    * update router
    
    * add pipfile
    
    * move to Pipfile
    
    * clean up
    
    * refactor
    
    * add github workflow

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+from .routers import strategy1_api
+
+app = FastAPI()
+
+app.include_router(strategy1_api.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Pong"}
